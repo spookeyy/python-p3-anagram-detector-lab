07995f8db55d88560dc8caeedc59bb03552166b3
spookeyy <pangasmeshack@gmail.com>
2024-05-26 14:06:02 +0300

    pass

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+# anagram.py
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        self.matches = []
+
+    def match(self, words):
+        for word in words:
+            if self.word != word and sorted(self.word) == sorted(word):
+                self.matches.append(word)
+        return self.matches
